150cdef76562976f9562e8c2a8bda869ebc22dc4
serious win <serious_ang@mail.ru>
2024-01-11 11:52:17 +0300

    version 2.0.0 created

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/donserious/version
+module github.com/donserious/version/v2
 
 go 1.20
--- version.go
+++ version.go
@@ -1,8 +1,6 @@
 package version
 
-import "fmt"
-
 // Version - print current module version
-func Version() {
-	fmt.Println("v1.0.2")
+func Version() string {
+	return "v2.0.0"
 }
